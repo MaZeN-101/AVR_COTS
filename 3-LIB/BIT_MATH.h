e2c0af54d1fc883dcfe005e59901900d9bbac42e
_MaZeN_ <85368475+MaZeN-101@users.noreply.github.com>
2022-09-22 18:47:58 +0200

    Add files via upload

---
+++ 3-LIB/BIT_MATH.h
@@ -0,0 +1,17 @@
+
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+//function like macro 
+//macros 
+
+#define SET_BIT(VAR,BIT_NO)    		VAR|=(1<<BIT_NO)
+
+#define CLR_BIT(VAR,BIT_NO)    		VAR&=(~(1<<BIT_NO))
+
+#define TOGGLE_BIT(VAR,BIT_NO) 		VAR^=(1<<BIT_NO)
+
+#define GET_BIT(VAR,BIT_NO)   		((VAR>>BIT_NO)&1)
+
+#endif
